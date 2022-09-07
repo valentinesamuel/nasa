0d1b9a3d66ea12535cb885cb2b65f4453037b772
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-07 14:24:13 +0100

    🧠 still finding the bug

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -4,22 +4,34 @@ async function httpGetPlanets() {
   // TODO: Once API is ready.
   // Load planets and return as JSON.
   const response = await fetch(`${API_URL}/planets`)
-return await response.json()
+  return await response.json()
 }
 
 async function httpGetLaunches() {
   // TODO: Once API is ready.
   // Load launches, sort by flight number, and return as JSON.
   const response = await fetch(`${API_URL}/launches`)
-  const fetchedLaunches =  await response.json()
+  const fetchedLaunches = await response.json()
   return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber
   })
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  console.log(launch);
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      header: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch)
+    })
+  } catch (err) {
+    return {
+      ok: false
+    }
+  }
 }
 
 async function httpAbortLaunch(id) {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getAllLaunches,addNewLaunch } = require('../../models/launches.model')
+const { getAllLaunches, addNewLaunch } = require('../../models/launches.model')
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches())
@@ -6,20 +6,20 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
     const launch = req.body
-
-    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+    if (!launch.mission || !launch.rocket || !launch.launchDate
+        || !launch.target) {
         return res.status(400).json({
-            error:"Missing requires launch property"
-        })
+            error: 'Missing required launch property',
+        });
     }
 
-    launch.launchDate = new Date(launch.launchDate)
+    launch.launchDate = new Date(launch.launchDate);
     if (isNaN(launch.launchDate)) {
         return res.status(400).json({
-    error: 'Invalid launch date'
-})
+            error: 'Invalid launch date',
+        });
     }
-    
+
     addNewLaunch(launch)
     return res.status(201).json(launch)
 }

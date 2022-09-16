be41abcd896b9c07e7f075b0a62765cb97f160d0
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-16 20:03:39 +0100

    removed open handles

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,18 +4,7 @@ const planets = require('./planets.mongo')
 
 const DEFAULT_FLIGHT_NUMBER = 100
 
-// const launch = {
-//     flightNumber: 100,
-//     mission: 'Kepler Exploration X',
-//     rocket: 'Explorer IS1',
-//     launchDate: new Date('December 27, 2030'),
-//     target: 'Kepler-442 b',
-//     customers: ['ZTM', 'NASA'],
-//     upcoming: true,
-//     success: true,
-// }
 
-// saveLaunch(launch)
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query'
 
 async function populateLaunches() {

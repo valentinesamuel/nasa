6d1a701a15c793c3159f1a7ad7ecf7dc9bb20c80
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-16 18:28:46 +0100

    Set default flight number

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const launchesDatabase = require('./launches.mongo')
 const planets = require('./planets.mongo')
 
-// const DEFAULT_FLIGHT_NUMBER = 100
+const DEFAULT_FLIGHT_NUMBER = 100
 
 // const launch = {
 //     flightNumber: 100,

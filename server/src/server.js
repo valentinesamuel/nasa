0fad64b1b89c845cb345f257cc7229d6386ca3a3
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-19 02:59:48 +0100

    switched to ubuntu vm

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const app = require('./app')
 const { mongoConnect } = require('./services/mongo')
 const { loadPlanetsData } = require('./models/planets.model')
 const { loadLaunchData } = require('./models/launches.model')
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app)
 
